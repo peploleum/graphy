382120650437b31ee1b97d92682b4b4b413f073a
peploleum <peploleum@gmail.com>
2018-12-20 10:27:18 +0100

    initialized Spring data configuration

---
+++ src/main/java/com/peploleum/insight/graphy/GraphyApplication.java
@@ -0,0 +1,14 @@
+package com.peploleum.insight.graphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphyApplication.class, args);
+    }
+
+}
+
