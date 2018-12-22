c4fc272375ae1134309a108156aa05c984d2f3ef
peploleum <peploleum@gmail.com>
2018-12-22 14:34:31 +0100

    fixed jib created graphy container

--- src/main/jib/entrypoint.sh
+++ src/main/jib/entrypoint.sh
@@ -1,4 +1,2 @@
-#!/bin/sh
-
 echo "The application is starting: ${GRAPHY_LABEL} with parameters: ${ENDPOINT} ${PORT} ${VERTEX_THRESHOLD}"
 exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.peploleum.insight.graphy.GraphyApplication"  "$@"
