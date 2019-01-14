ffbec410b8be43da28dd85c236d7ef65365dd01e
peploleum <peploleum@gmail.com>
2019-01-14 14:52:53 +0100

    cleaned networks before running other CRUD Units.

--- src/test/java/com/peploleum/insight/graphy/GraphyApplicationTests.java
+++ src/test/java/com/peploleum/insight/graphy/GraphyApplicationTests.java
@@ -1,7 +1,9 @@
 package com.peploleum.insight.graphy;
 
+import com.peploleum.insight.graphy.repository.NetworkRepository;
 import com.peploleum.insight.graphy.service.*;
 import com.peploleum.insight.graphy.web.rest.Type;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,12 +37,18 @@ public class GraphyApplicationTests {
     @Autowired
     private LocationServiceImpl locationService;
 
-    @Test
-    public void contextLoads() {
+    @Autowired
+    private NetworkRepository networkRepository;
+
+    @Before
+    public void setup() {
+        this.networkRepository.deleteAll();
     }
 
     @Test
     public void relationCreateTest() {
+
+        //inner ring
         final String biographicsMongoId = UUID.randomUUID().toString();
         final Long biographicsId = this.biographicsService.save("Paul", biographicsMongoId);
         final String rawDataMongoId = UUID.randomUUID().toString();
@@ -54,12 +62,38 @@ public class GraphyApplicationTests {
         final String locationMongoId = UUID.randomUUID().toString();
         final Long locationId = this.locationService.save("Paris", locationMongoId);
 
+        //outer ring
+        final String outerBiographicsMongoId = UUID.randomUUID().toString();
+        final Long outerBiographicsId = this.biographicsService.save("John", outerBiographicsMongoId);
+        final String outerRawDataMongoId = UUID.randomUUID().toString();
+        final Long outerRawDataId = this.rawDataService.save("CNN", outerRawDataMongoId, "RSS");
+        final String outerOrganisationMongoId = UUID.randomUUID().toString();
+        final Long outerOrganisationId = this.organisationService.save("ISIS", outerOrganisationMongoId, "Ismalic State");
+        final String outerEquipementMongoId = UUID.randomUUID().toString();
+        final Long outerEquipmentId = this.equipmentService.save("RPG", outerEquipementMongoId);
+        final String outerEventMongoId = UUID.randomUUID().toString();
+        final Long outerEventId = this.eventService.save("Bombing", outerEventMongoId);
+        final String outerLocationMongoId = UUID.randomUUID().toString();
+        final Long outerLocationId = this.locationService.save("London", outerLocationMongoId);
+
+        //inner ring
         this.relationService.save(rawDataId, biographicsId, "linked to", Type.RawData, Type.Biographics);
         this.relationService.save(rawDataId, eventId, "linked to", Type.RawData, Type.Event);
         this.relationService.save(rawDataId, organisationId, "linked to", Type.RawData, Type.Organisation);
         this.relationService.save(rawDataId, equipmentId, "linked to", Type.RawData, Type.Equipment);
         this.relationService.save(rawDataId, locationId, "linked to", Type.RawData, Type.Location);
 
+        //outer ring
+        this.relationService.save(biographicsId, outerBiographicsId, "linked to", Type.Biographics, Type.Biographics);
+        this.relationService.save(organisationId, outerBiographicsId, "linked to", Type.Organisation, Type.Biographics);
+        this.relationService.save(organisationId, outerEventId, "linked to", Type.Organisation, Type.Event);
+        this.relationService.save(eventId, outerOrganisationId, "linked to", Type.Event, Type.Organisation);
+        this.relationService.save(equipmentId, outerEquipmentId, "linked to", Type.Equipment, Type.Equipment);
+        this.relationService.save(locationId, outerOrganisationId, "linked to", Type.Location, Type.Organisation);
+        this.relationService.save(locationId, outerEventId, "linked to", Type.Location, Type.Event);
+        this.relationService.save(locationId, outerEquipmentId, "linked to", Type.Location, Type.Equipment);
+        this.relationService.save(locationId, outerBiographicsId, "linked to", Type.Location, Type.Biographics);
+        this.relationService.save(locationId, outerLocationId, "linked to", Type.Location, Type.Location);
     }
 
 }
