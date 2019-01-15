94259051ede73bb0203274d73bc2893479fb94b2
peploleum <peploleum@gmail.com>
2019-01-15 14:22:10 +0100

    deleted comments

--- src/main/java/com/peploleum/insight/graphy/domain/Biographics.java
+++ src/main/java/com/peploleum/insight/graphy/domain/Biographics.java
@@ -1,37 +1,20 @@
 package com.peploleum.insight.graphy.domain;
 
-import com.microsoft.spring.data.gremlin.annotation.GeneratedValue;
 import com.microsoft.spring.data.gremlin.annotation.Vertex;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-/*import org.springframework.data.elasticsearch.annotations.FieldType;
-import org.springframework.data.elasticsearch.annotations.Mapping;
-import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DBRef;
-import javax.validation.constraints.*;*/
 
 import java.util.Objects;
 
-//import com.peploleum.insight.domain.enumeration.Gender;
-
-/**
- * A Biographics.
- */
 @Data
 @Vertex
 @AllArgsConstructor
 @NoArgsConstructor
-public class Biographics extends DataJanus{
+public class Biographics extends DataJanus {
 
-    //@org.springframework.data.elasticsearch.annotations.Field(type = FieldType.Keyword)
-    //@Mapping(mappingPath = "/mappings/asset_id_mapping.json")
-    //@Field("biographics_name")
     private String biographicsName;
 
-    //@Field("biographics_age")
     private Integer biographicsAge;
 
 
@@ -62,8 +45,6 @@ public class Biographics extends DataJanus{
     }
 
 
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
-
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -87,9 +68,9 @@ public class Biographics extends DataJanus{
     @Override
     public String toString() {
         return "Biographics{" +
-            "id=" + getId() +
-            ", biographicsName='" + getBiographicsName() + "'" +
-            ", biographicsAge=" + getBiographicsAge() +
-            "}";
+                "id=" + getId() +
+                ", biographicsName='" + getBiographicsName() + "'" +
+                ", biographicsAge=" + getBiographicsAge() +
+                "}";
     }
 }
