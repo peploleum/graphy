56516644be3b39e6c2c54a2c6b0add37b66c027d
peploleum <peploleum@gmail.com>
2019-01-20 21:41:43 +0100

    fixed build script for windows

--- custom_build.bat
+++ custom_build.bat
@@ -1,3 +1,3 @@
-REM call mvn package -DskipTests verify jib:dockerBuild
+call mvn package -DskipTests verify jib:dockerBuild
 call docker tag graphy peploleum/graphy:1.0.0
 call docker push peploleum/graphy:1.0.0
